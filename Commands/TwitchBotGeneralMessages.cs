1e347f870bef00a423001ea2b63931b796ee3dad
Unknown <b.regan8@hotmail.com>
2018-06-19 21:55:34 +0100

    Activated bit thanks

--- Commands/TwitchBotGeneralMessages.cs
+++ Commands/TwitchBotGeneralMessages.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Bregan_TwitchBot.Commands.Message_Limiter;
 using Bregan_TwitchBot.Connection;
+using TwitchLib.PubSub.Events;
 
 namespace Bregan_TwitchBot.Commands
 {
@@ -14,44 +15,36 @@ namespace Bregan_TwitchBot.Commands
             TwitchBotConnection.Client.OnNewSubscriber += NewSub;
             TwitchBotConnection.Client.OnReSubscriber += Resub;
             TwitchBotConnection.Client.OnGiftedSubscription += GiftSub;
-            TwitchBotConnection.Client.OnBeingHosted += Client_OnBeingHosted;
-            //PubSubConnection.pubSubClient.OnBitsReceived += BitsDonated;
-        }
-
-        private static void Client_OnBeingHosted(object sender, TwitchLib.Client.Events.OnBeingHostedArgs e)
-        {
-            Console.WriteLine("TEST");
+            PubSubConnection.PubSubClient.OnBitsReceived += BitsDonated;
         }
 
         //Gifted subs
         private static void GiftSub(object sender, TwitchLib.Client.Events.OnGiftedSubscriptionArgs e)
         {
-            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, $"Thank you {e.GiftedSubscription.DisplayName} for gifting {e.GiftedSubscription.MsgParamRecipientUserName} a sub PogChamp <3 <3 <3 blocksWOT");
+            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, $"Thank you {e.GiftedSubscription.DisplayName} for gifting {e.GiftedSubscription.MsgParamRecipientUserName} a sub PogChamp <3 <3 <3");
         }
         //Resubscriptions
         private static void Resub(object sender, TwitchLib.Client.Events.OnReSubscriberArgs e)
         {
-            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, $"Welcome back {e.ReSubscriber.DisplayName} for {e.ReSubscriber.Months} months with the message {e.ReSubscriber.ResubMessage} <3 <3 <3 <3 blocksWOT");
+            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, $"Welcome back {e.ReSubscriber.DisplayName} for {e.ReSubscriber.Months} months with the message {e.ReSubscriber.ResubMessage} <3 <3 <3 <3");
         }
         //New subscription
         private static void NewSub(object sender, TwitchLib.Client.Events.OnNewSubscriberArgs e)
         {
-            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, $"Welcome {e.Subscriber.DisplayName} to the {StartService.ChannelName} squad! blocksWOT");
+            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, $"Welcome {e.Subscriber.DisplayName} to the {StartService.ChannelName} squad!");
         }
         //Connection
         private static void BotConnectedToChannel(object sender, TwitchLib.Client.Events.OnConnectedArgs e)
         {
-            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, "Successfully connected blocksWOT");
+            TwitchBotConnection.Client.SendMessage(StartService.ChannelName, "Successfully connected");
             Console.WriteLine("[Twitch Connection] Bot Sucessfully connected");
         }
 
-        
-        //Bits donations --------------- NEEDS auth from channel owner
-//        private static void BitsDonated(object sender, OnBitsReceivedArgs e)
-//        {
-//            Console.WriteLine($"Just received {e.BitsUsed} bits from {e.Username}. That brings their total to {e.TotalBitsUsed} bits!");
-//            TwitchBotConnection.client.SendMessage(StartService.ChannelName, $"hoooo {e.Username} has donated + {e.BitsUsed} with a grand total of {e.TotalBitsUsed} donated PogChamp");
-//        }
+      
+        private static void BitsDonated(object sender, OnBitsReceivedArgs e)
+        {
+            Console.WriteLine($"Just received {e.BitsUsed} bits from {e.Username}. That brings their total to {e.TotalBitsUsed} bits!");/            TwitchBotConnection.client.SendMessage(StartService.ChannelName, $"{e.Username} has donated + {e.BitsUsed} with a grand total of {e.TotalBitsUsed} donated PogChamp");
+        }
 
     }
 }
